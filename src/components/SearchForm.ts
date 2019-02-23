efeecae8c5cffa3f5198b469e761b111c0331e9f
James Thompson <thisguy@jimmythompson.me>
2019-02-23 14:01:19 +0000

    wip

--- src/components/SearchForm.ts
+++ src/components/SearchForm.ts
@@ -1,15 +1,22 @@
 import { LitElement, html } from 'lit-element';
 import { isValidSearchRequest, makeRequest } from '../geocoding';
+import '../styles/_search-form.scss';
 
+const exampleResponse = require('../geocodingResponse.json');
+
+console.log(exampleResponse);
 class SearchForm extends LitElement {
 
   private errorMessage: string;
   private inputValue: string;
+  private timeout: number; // The timeout ID for the search input debouncer
+  private suggestions: any[];
 
   static get properties() {
     return { 
       errorMessage: { type: String },
       inputValue: { type: String },
+      suggestions: { type: Array },
     };
   }
 
@@ -17,29 +24,35 @@ class SearchForm extends LitElement {
     super();
     this.errorMessage = '';
     this.inputValue = '';
+    this.suggestions = exampleResponse.features;
   }
 
 
   formSubmit(e: any) {
     e.preventDefault();
-
-    const value = this.inputValue;
-
-    this.errorMessage = '';
-
-    const validRequest = isValidSearchRequest(value);
-    if (validRequest !== true) this.errorMessage = validRequest;
-
-    makeRequest(value)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch(console.log)
     
   }
 
   searchInput(e: KeyboardEvent) {
+    clearTimeout(this.timeout);
     this.inputValue = (e.target as HTMLInputElement).value;
+
+    const value = this.inputValue;
+
+    this.errorMessage = '';
+    this.suggestions = [];
+
+    const validRequest = isValidSearchRequest(value);
+    if (validRequest !== true) return this.errorMessage = validRequest;
+
+    this.timeout = window.setTimeout(() => {
+      makeRequest(value)
+      .then((res) => {
+        this.suggestions = res.features;
+      })
+      .catch(console.log)
+    }, 300);
+    
   }
 
   render() {
@@ -48,7 +61,11 @@ class SearchForm extends LitElement {
         ${this.errorMessage}
         <input type="search" class="search-form__input" @input="${this.searchInput}" value="${this.inputValue}"/>
         <button type="submit" class="search-form__submit">Search</button>
-        <div class="search-form__options"></div>
+        <ul class="search-form__options">
+          ${this.suggestions.map(({ text }) => {
+            return html`<li class="search-form__options__item">${text}</li>`;
+          })}
+        </ul>
       </form>
     `;
   }
