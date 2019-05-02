545eed422b22b9bd10a157d7c2ad996d21307089
jimmaaay <thisguy@jimmythompson.me>
2019-05-02 13:41:00 +0100

    split bounding box WIP

--- src/policeAPI.ts
+++ src/policeAPI.ts
@@ -1,40 +1,70 @@
 import haversine from 'haversine';
 
-export const getCrimesByBbox = (bbox: any, { month, year }: any) => {
+const ENDPOINT = 'https://data.police.uk/api/crimes-street/all-crime';
 
-  const points = [
-    [bbox[0], bbox[1]],
-    [bbox[2], bbox[1]],
-    [bbox[2], bbox[3]],
-    [bbox[0], bbox[3]],
-  ];
-  
-  const xStart = { longitude: bbox[0], latitude: bbox[1] };
-  const xEnd = { longitude: bbox[2], latitude: bbox[1] };
-  const yStart = { longitude: bbox[0], latitude: bbox[1] };
-  const yEnd = { longitude: bbox[0], latitude: bbox[3] };
+export const getCrimesByBbox = async (bbox: any, { month, year }: any) => {
 
-  const lngMiles = haversine(xStart, xEnd, { unit: 'mile' });
-  const latMiles = haversine(yStart, yEnd, { unit: 'mile' });
+  const getMilesFromLatLngs = (boundingBox: any) => {
+    const xStart = { longitude: boundingBox[0], latitude: boundingBox[1] };
+    const xEnd = { longitude: boundingBox[2], latitude: boundingBox[1] };
+    const yStart = { longitude: boundingBox[0], latitude: boundingBox[1] };
+    const yEnd = { longitude: boundingBox[0], latitude: boundingBox[3] };
+    const lngMiles = haversine(xStart, xEnd, { unit: 'mile' });
+    const latMiles = haversine(yStart, yEnd, { unit: 'mile' });
 
-  console.log(lngMiles, latMiles);
-
-  if (lngMiles > 8 || latMiles > 8) {
-    console.warn('Should cut up into segments');
+    return {
+      lngMiles: haversine(xStart, xEnd, { unit: 'mile' }),
+      latMiles: haversine(yStart, yEnd, { unit: 'mile' }),
+    };
   }
 
-  const poly = points.map(([ lng, lat ]) => {
-    return `${lat},${lng}`
-  }).join(':');
+  const getData = (boundingBox: any) => {
+    const points = [
+      [bbox[0], bbox[1]],
+      [bbox[2], bbox[1]],
+      [bbox[2], bbox[3]],
+      [bbox[0], bbox[3]],
+    ];
+    const poly = points.map(([ lng, lat ]) => {
+      return `${lat},${lng}`
+    }).join(':');
+  
+  
+    const searchParams = new URLSearchParams({
+      poly,
+      date: `${year}-${formattedMonth}`,
+    });
 
+    return fetch(`${ENDPOINT}?${searchParams.toString()}`)
+      .then((response) => {
+        if (response.status !== 200) throw new Error(response.status.toString());
+        return response.json();
+      });
+  };
+
+  const splitBoundingBox = (boundingBox: any, numberToSplitBy: number) => {
+    if (numberToSplitBy === 0) return boundingBox;
+    const [ left, top, right, bottom ] = boundingBox;
+    const middleX = right - left / 2;
+    const middleY = top - bottom / 2;
+    
+    // const newBboxArray = righ
+  };
+  
+  
   const formattedMonth = (month + 1).toString().padStart(2, '0');
-  const endpoint = 'https://data.police.uk/api/crimes-street/all-crime';
-  const searchParams = new URLSearchParams({
-    poly,
-    date: `${year}-${formattedMonth}`,
-  });
+  
 
-  return fetch(`${endpoint}?${searchParams.toString()}`)
-    .then(_ => _.json())
+  let isGood = false;
+  let i = 0;
+  while (!isGood) {
+    const boundingBox = splitBoundingBox(bbox, i);
+    const { lngMiles, latMiles } = getMilesFromLatLngs(bbox);
+    i++;
+    // if (lngMiles )
+  }
+
+
+  // return getData(bbox);
 
 }
