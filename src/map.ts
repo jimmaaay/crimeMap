d2df76a1dca192a56eea240bea7399b5c276ca36
James Thompson <thisguy@jimmythompson.me>
2019-07-06 00:39:40 +0100

    Added click to zoom on marker clusters

--- src/map.ts
+++ src/map.ts
@@ -1,5 +1,6 @@
 import * as mapboxgl from 'mapbox-gl';
 import { LngLatBoundsLike } from 'mapbox-gl';
+import v4 from 'uuid';
 import haversine from 'haversine';
 import markerUrl from './marker.png';
 import { store } from './store';
@@ -7,7 +8,106 @@ import { MapMarker } from './types';
 
 (mapboxgl as any).accessToken = process.env.MAPBOX_ACCESS_TOKEN;
 
+interface GroupArgs {
+  initialPosition: any;
+  map: mapboxgl.Map;
+}
 
+const CLUSTER_RADIUS = 40;
+
+class Group {
+  public markers: any[];
+  public initialPosition: any;
+  public bounds: mapboxgl.LngLatBounds;
+  private map: mapboxgl.Map;
+  public id: string;
+
+  public markerMaxBounds: any;
+
+  constructor(options: GroupArgs) {
+    const { initialPosition, map } = options;
+    this.markers = [];
+    this.initialPosition = initialPosition;
+    this.map = map;
+    this.id = v4();
+    this.markerMaxBounds = {
+      east: this.initialPosition.lng,
+      west: this.initialPosition.lng,
+      north: this.initialPosition.lat,
+      south: this.initialPosition.lat,
+    };
+
+    this.getBounds();
+  }
+
+  addMarker(marker: MapMarker) {
+    this.markers.push(marker);
+
+    if (this.markerMaxBounds.east < marker.lng) {
+      this.markerMaxBounds.east = marker.lng;
+    }
+
+    if (this.markerMaxBounds.west > marker.lng) {
+      this.markerMaxBounds.west = marker.lng;
+    }
+
+    if (this.markerMaxBounds.north < marker.lat) {
+      this.markerMaxBounds.north = marker.lat;
+    }
+
+    if (this.markerMaxBounds.south > marker.lat) {
+      this.markerMaxBounds.south = marker.lat;
+    }
+  }
+
+  getMarkerBounds() {
+    return new mapboxgl.LngLatBounds(
+      [this.markerMaxBounds.west, this.markerMaxBounds.south],
+      [this.markerMaxBounds.east, this.markerMaxBounds.north],
+    );
+  }
+
+  getBounds() {
+    // Convert latLng to pixels
+    const point = this.map.project({
+      lat: this.initialPosition.lat,
+      lon: this.initialPosition.lng,
+    });
+
+    const radius = CLUSTER_RADIUS * 2 * 1.25;
+    
+    const northEastPoint = {
+      x: point.x + radius,
+      y: point.y - radius,
+    } as mapboxgl.PointLike;
+
+    const southWestPoint = {
+      x: point.x - radius,
+      y: point.y + radius,
+    } as mapboxgl.PointLike;
+
+    const bounds = new mapboxgl.LngLatBounds(
+      this.map.unproject(southWestPoint), // convert back to latLng
+      this.map.unproject(northEastPoint), // convert back to latLng
+    );
+
+    this.bounds = bounds;
+  }
+
+  shouldMarkerBeInGroup(marker: MapMarker) {
+    const north = this.bounds.getNorth();
+    const east = this.bounds.getEast();
+    const south = this.bounds.getSouth();
+    const west = this.bounds.getWest();
+
+    return (
+      marker.lng <= east
+      && marker.lng >= west
+      && marker.lat >= south
+      && marker.lat <= north
+    );
+  }
+}
 
 
 const createColouredMarker = (
@@ -36,6 +136,20 @@ export default async () => {
   image.onload = () => imageLoaded = true;
   image.src = markerUrl;
 
+  const canvas = document.createElement('canvas');
+  canvas.width = CLUSTER_RADIUS * 2
+  canvas.height = CLUSTER_RADIUS * 2;
+
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.arc(CLUSTER_RADIUS, CLUSTER_RADIUS, CLUSTER_RADIUS, 0, Math.PI * 2);
+  ctx.fillStyle = 'rgb(255, 0, 0)';
+  ctx.fill();
+  ctx.closePath();
+
+  const redCircleImageData = ctx.getImageData(0, 0, CLUSTER_RADIUS * 2, CLUSTER_RADIUS * 2);
+
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
@@ -45,6 +159,12 @@ export default async () => {
       54.620976,
     ],
   });
+
+  map.addImage(
+    `cluster-red`,
+    { width: CLUSTER_RADIUS * 2, height: CLUSTER_RADIUS * 2, data: redCircleImageData.data },
+    // { pixelRatio: 2 },
+  );
 
   const existingMarkers: string[] = [];
 
@@ -97,9 +217,49 @@ export default async () => {
   }
 
 
+  // let drawGroupsCalled = fals
+
+  const drawGroups = (groups: Group[]) => {
+    const source = map.getSource('markers') as mapboxgl.GeoJSONSource || undefined;
+    const markerSourceData: any = {
+      type: 'FeatureCollection',
+      features: groups.map((group, i) => {
+        const { lng, lat } = group.initialPosition;
+        return {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat]
+          },
+          properties: {
+            id: group.id,
+            'marker-image': 'red',
+            title: group.markers.length.toString(),
+          },
+        };
+        // { lat, lng, category, persistendID }
+        // return {
+        //   type: 'Feature',
+        //   geometry: {
+        //     type: 'Point',
+        //     coordinates: [lng, lat]
+        //   },
+        //   properties: {
+        //     persistendID,
+        //     'marker-category': category,
+        //   },
+        // };
+      }),
+    };
+
+    source.setData(markerSourceData);
+  
+  };
+
 
 
   let currentMarkers: MapMarker[] = [];
+  let currentGroups: Group[] = [];
   /**
    * Will add an array of markers to the map.
    * 
@@ -135,25 +295,14 @@ export default async () => {
     currentMarkers = markers;
 
     const groups = getMarkerGroups(markers);
+    currentGroups = groups;
     markersToShow = groups;
 
     const layerExists = map.getLayer('markers') !== undefined;
     const source = map.getSource('markers') as mapboxgl.GeoJSONSource || undefined;
     const markerSourceData: any = {
       type: 'FeatureCollection',
-      features: markersToShow.map(({ lat, lng, category, persistendID }, i) => {
-        return {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [lng, lat]
-          },
-          properties: {
-            persistendID,
-            'marker-category': category,
-          },
-        };
-      }),
+      features: [],
     };
 
     if (source === undefined) {
@@ -162,9 +311,9 @@ export default async () => {
         data: markerSourceData,
       };
       map.addSource('markers', markerSource);
-    } else {
-      source.setData(markerSourceData);
     }
+
+    drawGroups(groups);
 
     if (!layerExists) {
       map.addLayer({
@@ -172,80 +321,28 @@ export default async () => {
         type: 'symbol',
         source: 'markers',
         layout: {
-          'icon-image': 'category-{marker-category}',
+          'icon-image': 'cluster-{marker-image}',
           'icon-allow-overlap': true,
           'icon-ignore-placement': true,
+          'text-field': '{title}',
+          // "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          // "text-offset": [0, 0.6],
+          // "text-anchor": "top"
         },
       });
     }
 
   }
 
-  const getMarkerGroups = (markers: MapMarker[]) => {
+  const getMarkerGroups = (markers: MapMarker[]): Group[] => {
     const bounds = map.getBounds();
-    const GROUP_WIDTH = 100;
+    
 
   
     const groups: Group[] = [];
 
     // console.log(bounds, window.innerWidth);
 
-    class Group {
-      public markers: any[];
-      public initialPosition: any;
-      private bounds: mapboxgl.LngLatBounds;
-
-      constructor(initialPosition: any) {
-        this.markers = [];
-        this.initialPosition = initialPosition;
-        this.getBounds();
-      }
-
-      addMarker(marker: MapMarker) {
-        this.markers.push(marker);
-      }
-
-      getBounds() {
-        // Convert latLng to pixels
-        const point = map.project({
-          lat: this.initialPosition.lat,
-          lon: this.initialPosition.lng,
-        });
-
-        const radius = GROUP_WIDTH / 2;
-        
-        const northEastPoint = {
-          x: point.x - radius,
-          y: point.y - radius,
-        } as mapboxgl.PointLike;
-
-        const southWestPoint = {
-          x: point.x + radius,
-          y: point.y + radius,
-        } as mapboxgl.PointLike;
-
-        const bounds = new mapboxgl.LngLatBounds(
-          map.unproject(southWestPoint), // convert back to latLng
-          map.unproject(northEastPoint), // convert back to latLng
-        );
-
-        this.bounds = bounds;
-      }
-
-      shouldMarkerBeInGroup(marker: MapMarker) {
-        const north = this.bounds.getNorth();
-        const east = this.bounds.getEast();
-        const south = this.bounds.getSouth();
-        const west = this.bounds.getWest();
-
-        return (
-          marker.lng >= east
-          && marker.lng <= west
-          && marker.lat >= south
-          && marker.lat <= north
-        );
-      }
-    }
 
     for (let i = 0; i < markers.length; i++) {
       const marker = markers[i];
@@ -262,8 +359,11 @@ export default async () => {
 
       if (!hasBeenAddedToGroup) {
         const group = new Group({
-          lat: marker.lat,
-          lng: marker.lng,
+          map,
+          initialPosition: {
+            lat: marker.lat,
+            lng: marker.lng,
+          },
         });
 
         group.addMarker(marker);
@@ -272,17 +372,7 @@ export default async () => {
 
     }
 
-    const markersToShow = groups.map((group) => {
-      // console.log(`Group has ${group.markers.length} items`);
-      return {
-        lat: group.initialPosition.lat,
-        lng: group.initialPosition.lng,
-        category: 'burglary',
-        persistendID: (Math.random() * 10000).toString(),
-      };
-    });
-
-    return markersToShow;
+    return groups;
   }
 
 
@@ -291,33 +381,24 @@ export default async () => {
   map.on('zoom', () => {
     const zoom = Math.floor(map.getZoom());
     if (prevZoom === zoom) return;
-    if (currentMarkers.length === 0) return;
     prevZoom = zoom;
+    if (currentMarkers.length === 0) return;
 
     const groups = getMarkerGroups(currentMarkers);
-
-    const source = map.getSource('markers') as mapboxgl.GeoJSONSource || undefined;
-    const markerSourceData: any = {
-      type: 'FeatureCollection',
-      features: groups.map(({ lat, lng, category, persistendID }, i) => {
-        return {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [lng, lat]
-          },
-          properties: {
-            persistendID,
-            'marker-category': category,
-          },
-        };
-      }),
-    };
-
-    source.setData(markerSourceData);
-    
-    // console.log(zoom);
+    currentGroups = groups;
+    drawGroups(groups);
   });
+
+  map.on('click', 'markers', (e) => {
+    const clickedItem = e.features[0];
+    const { id: groupId } = clickedItem.properties;
+    const group = currentGroups.find(group => groupId === group.id);
+    if (group === undefined) throw new Error(`Cannot find group ${groupId}`);
+    map.fitBounds(group.getMarkerBounds(), {
+      padding: 50,
+    });
+  });
+
 
   return {
     drawBox,
